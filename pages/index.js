143216e10134aa83492c733f89d84306e3ff1699
Tan Kai Min Russell <russelltanreg38@gmail.com>
2024-08-21 10:29:26 +0800

    Add SEO and remove image

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ import Button from "../components/Button";
 import Link from "next/link";
 import Cursor from "../components/Cursor";
 import Image from "next/image";
+import { Helmet } from "react-helmet";
 
 // Local Data
 import data from "../data/portfolio.json";
@@ -69,42 +70,30 @@ export default function Home() {
             <div className="mt-5">
               <h1
                 ref={textOne}
-                className="text-2xl tablet:text-6xl laptop:text-6xl laptopl:text-3xl p-1 ml-1 tablet:p-2 text-bold w-4/5 mob:w-full laptop:w-4/5"
+                className="text-2xl tablet:text-4xl laptop:text-3xl laptopl:text-3xl p-1 ml-1 tablet:p-2 text-bold w-4/5 mob:w-full laptop:w-4/5"
               >
                 {data.headerTaglineOne}
               </h1>
               <h1
                 ref={textTwo}
-                className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-3xl p-1 ml-1 tablet:p-2 text-bold w-full laptop:w-4/5"
+                className="text-3xl tablet:text-4xl laptop:text-3xl laptopl:text-3xl p-1 ml-1 tablet:p-2 text-bold w-full laptop:w-4/5"
               >
                 {data.headerTaglineTwo}
               </h1>
               <h1
                 ref={textThree}
-                className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-3xl p-1 ml-1 tablet:p-2 text-bold w-full laptop:w-4/5"
+                className="text-3xl tablet:text-4xl laptop:text-3xl laptopl:text-3xl p-1 ml-1 tablet:p-2 text-bold w-full laptop:w-4/5"
               >
                 {data.headerTaglineThree}
               </h1>
               <h1
                 ref={textFour}
-                className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-3xl p-1 ml-1 tablet:p-2 text-bold w-full laptop:w-4/5"
+                className="text-3xl tablet:text-4xl laptop:text-3xl laptopl:text-3xl p-1 ml-1 tablet:p-2 text-bold w-full laptop:w-4/5"
               >
                 {data.headerTaglineFour}
               </h1>
             </div>
             <Socials className="mt-2 p-1 ml-1 laptop:mt-5" />
-          </div>
-
-          {/* Image Section */}
-          <div className="mt-5 laptop:mt-0 laptop:ml-2 w-1/3 tablet:ml-10 mr-5 w-full">
-            <Image 
-              src="/images/face.jpg" 
-              alt="My Face" 
-              layout="responsive"
-              width={500} 
-              height={500} 
-              className="rounded"
-            />
           </div>
         </div>
 
@@ -154,6 +143,14 @@ export default function Home() {
         )}
         <Footer />
       </div>
+      <Helmet>
+        <title>Russell Tan | Home</title>
+        <meta name='description' content="Home Page of Russell-Site"/>
+        <meta property="og:title" content={`Russell Tan | Home`}/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:description" content="Tan Kai Min, Russell's webpage"/>
+        <meta property="og:url" content="russelltankm.com"/>
+      </Helmet>
     </div>
   );
 }
