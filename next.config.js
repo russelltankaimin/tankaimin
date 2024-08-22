7dac10e0c9e3bf8a4eb96286b1d2fa94ca3b2266
Tan Kai Min Russell <russelltanreg38@gmail.com>
2024-08-22 15:58:26 +0800

    Add redirects

--- next.config.js
+++ next.config.js
@@ -1,5 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/aboutme/:slug',
+        destination: '/resume',
+        permanent: true,
+      },
+      // Wildcard path matching
+      {
+        source: '/docs/:slug',
+        destination: '/blog',
+        permanent: true,
+      },
+      {
+        source: '/blog/uni/:slug',
+        destination: '/blog',
+        permanent: true,
+      },
+    ]
+  },
   reactStrictMode: true,
 }
 
